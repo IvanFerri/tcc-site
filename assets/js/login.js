82fcc04e7ec8aa720a621f614f162bdf697828cc
IvanFerri <ivanmferri@gmail.com>
2025-12-16 21:09:47 -0400

    Refactor login function to use Firebase Auth

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,16 @@
-function login() {
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+window.login = function () {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
 
-  auth.signInWithEmailAndPassword(email, senha)
+  signInWithEmailAndPassword(auth, email, senha)
     .then(() => {
       window.location.href = "admin.html";
     })
     .catch(() => {
       alert("Usuário ou senha inválidos");
     });
-}
+};
