e02c76bc3271dab597e674b2a44536c48f5ae82b
IvanFerri <ivanmferri@gmail.com>
2025-12-16 21:09:16 -0400

    Refactor Firebase initialization and service exports

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,3 +1,9 @@
+// Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+// CONFIGURAÇÃO DO SEU FIREBASE
 const firebaseConfig = {
   apiKey: "SUA_API_KEY",
   authDomain: "grafica-vip-tcc.firebaseapp.com",
@@ -7,7 +13,9 @@ const firebaseConfig = {
   appId: "XXXX"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Serviços
+export const auth = getAuth(app);
+export const db = getFirestore(app);
