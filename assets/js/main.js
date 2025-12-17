8c2c180c134bde91de57d6da066afd99e9bb02fe
IvanFerri <ivanmferri@gmail.com>
2025-12-16 21:40:59 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,14 @@
 import { db } from "./firebase.js";
-import {
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { collection, getDocs } from
+  "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+const lista = document.getElementById("produtos-list");
 
 async function carregarProdutos() {
-  const container = document.getElementById("produtos");
-
   const snapshot = await getDocs(collection(db, "produtos"));
   snapshot.forEach(doc => {
     const p = doc.data();
-    container.innerHTML += `
+    lista.innerHTML += `
       <div class="produto">
         <img src="${p.imagem}">
         <h3>${p.nome}</h3>
@@ -19,5 +17,10 @@ async function carregarProdutos() {
     `;
   });
 }
+
+document.getElementById("whatsapp-btn").href =
+  "https://wa.me/5567999883923";
+document.getElementById("whatsapp-btn2").href =
+  "https://wa.me/5567999883923";
 
 carregarProdutos();
